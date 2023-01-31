9ad0e47620f60cb63a812f218a35ddf882f2de89
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-01-31 18:52:11 -0500

    api works

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const blogRoutes = require('./blogRoutes');
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+router.use('/', blogRoutes);
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
- res.send("respond with a resource");
-  });
+  res.send("respond with a resource");
+});
 
-router.use('/blogs', blogRoutes);
-
+// router.use('/blogs', blogRoutes);
 
 module.exports = router;
